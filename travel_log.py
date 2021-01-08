64be436e6614d34e74663d55dd67c08554acf834
Alex Combas <alex@flyingcastle.net>
2021-01-08 13:04:04 -0800

    Finished travel_log

--- travel_log.py
+++ travel_log.py
@@ -11,7 +11,15 @@ travel_log = [
 },
 ]
 def add_new_country(country, visits, cities):
-    travel_log.append({"country":country, "visits": visits, "cities": cities})
+    # more verbose but easier to understand maybe
+    new_country = {}
+    new_country["country"] = country
+    new_country["visits"] = visits
+    new_country["cities"] = cities
+    travel_log.append(new_country)
+
+    # easier in a single line
+    #travel_log.append({"country":country, "visits": visits, "cities": cities})
 
 
 add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
