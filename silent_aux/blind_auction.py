c4b3be263a5bbbdc29400e54276e10d120ccee07
Alex Combas <alex@flyingcastle.net>
2021-01-08 13:44:26 -0800

    Start of Silent auction

---
+++ silent_aux/blind_auction.py
@@ -0,0 +1,40 @@
+logo = '''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+'''
+import os
+# how to clear the screen on windows and nix
+#os.system('cls' if os.name == 'nt' else 'clear')
+
+bidders = {}
+
+print(logo)
+print("Welcome to the silent auction program.")
+
+more_bidders = "yes"
+
+while (more_bidders == "yes"):
+
+    name = input("What is your name: ")
+    bid = int(input("What is your bid: "))
+    bidders[name] = bid
+    more_bidders = input("Are there any other bidders? Type 'yes' or 'no'. ")
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+winner_name = ""
+winner_bid = 0
+for key in bidders:
+    if bidders[key] > winner_bid:
+        winner_name = key
+        winner_bid = bidders[key]
+
+print(f"The winner is {winner_name} with a bid of {winner_bid}")
