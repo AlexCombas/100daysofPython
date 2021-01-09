a7616990e3ba03aadf4355b086cacb5d075d5339
Alex Combas <alex@flyingcastle.net>
2021-01-08 20:57:22 -0800

    Finished the calc program

---
+++ functions/function1.py
@@ -0,0 +1,7 @@
+def format_name(f_name, l_name):
+    fmt_f_name = f_name.title()
+    fmt_l_name = l_name.title()
+    
+    return f"{fmt_f_name} {fmt_l_name}"
+
+print(format_name("alex", "cOmBaS"))  
